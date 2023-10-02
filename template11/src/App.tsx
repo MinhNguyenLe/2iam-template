2398e409659cfbe4052a459fae07b3d936838a48
Minh <leminh.nguyen@btaskee>
2023-10-02 23:04:57 +0700

    feat: call db template erfjs

--- template11/src/App.tsx
+++ template11/src/App.tsx
@@ -7,17 +7,50 @@ import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "./components/Loader";
+import { GlobalData } from "./GlobalData";
+
 const App = () => {
+  const [globalData, setGlobalData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetch() {
+      return axios
+        .get("http://localhost:8080/api/erfjs", {
+          params: { userId: "abcd1234" },
+        })
+        .then((result) => {
+          setGlobalData(result.data.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsLoading(false);
+        });
+    }
+
+    fetch();
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/experience' element={<Experience />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <GlobalData.Provider value={globalData}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </GlobalData.Provider>
+      )}
     </>
   );
 };
--- template11/src/pages/Home/index.tsx
+++ template11/src/pages/Home/index.tsx
@@ -1,31 +1,59 @@
 const Zoom = require("react-reveal/Zoom");
 
-import { AboutData } from "../../data/AboutData";
-import { LinksData } from "../../data/LinksData";
 import styles from "./home.module.scss";
 import profilePic from "../../assets/profile.png";
 
+import { useContext } from "react";
+import { GlobalData } from "../../GlobalData";
+import { ImGithub, ImLinkedin } from "react-icons/im";
+import { FaTwitter } from "react-icons/fa";
+import { SiInstagram } from "react-icons/si";
+
 const Home = () => {
+  const data = useContext(GlobalData);
+
   return (
     <div className={styles.home}>
       <Zoom cascade>
         <img src={profilePic} alt="Profile" className={styles.image} />
-        <h2 className={styles.title}>I Am, Erfan Granmayeh`</h2>
+        <h2 className={styles.title}>{data.about_me.say_hi}</h2>
       </Zoom>
       <Zoom cascade>
-        <h3 className={styles.about}>{AboutData.about1}</h3>
-        <h3 className={styles.about_next}>{AboutData.about2}</h3>
+        <h3 className={styles.about}>{data.about_me.about_1}</h3>
+        <h3 className={styles.about_next}>{data.about_me.about_2}</h3>
         <div className={styles.links}>
-          {LinksData.map((item) => (
-            <a
-              href={item.link}
-              target="_blank"
-              rel="noreferrer"
-              key={item.title}
-            >
-              {item.icon}
-            </a>
-          ))}
+          <a
+            href={data.about_me.social_link.github}
+            target="_blank"
+            rel="noreferrer"
+            key="GitHub"
+          >
+            <ImGithub color="#000000dc" />
+          </a>
+          <a
+            href={data.about_me.social_link.linkedIn}
+            target="_blank"
+            rel="noreferrer"
+            key="LinkedIn"
+          >
+            <ImLinkedin color="#000000dc" />
+          </a>
+          <a
+            href={data.about_me.social_link.twitter}
+            target="_blank"
+            rel="noreferrer"
+            key="Twitter"
+          >
+            <FaTwitter color="#000000dc" />
+          </a>
+          <a
+            href={data.about_me.social_link.instagram}
+            target="_blank"
+            rel="noreferrer"
+            key="Instagram"
+          >
+            <SiInstagram color="#000000dc" />
+          </a>
         </div>
       </Zoom>
     </div>
